12e3b6ae2b02568d71519f4a59bebf6974cd8171
soufia97 <smaherzi@yahoo.fr>
2018-04-19 16:53:26 +0200

    Affichage de la BDD

--- src/Contrôleur/TestHopital.java
+++ src/Contrôleur/TestHopital.java
@@ -26,15 +26,14 @@ public class TestHopital {
     ResultSet myRs = null;
     public static void main(String[] args) throws SQLException, ClassNotFoundException{
     //Connexion cn = new Connexion(); 
-<<<<<<< HEAD
-    Connexion cn;
+
+ /** Connexion cn;
     cn = new Connexion("Hopital","root","");
     MenuConnexion mconnexion = new MenuConnexion();
     //FenetreConnexionLocale fe = new FenetreConnexionLocale();
     //System.out.println(afficherAttribut("medecin"));
-=======
    
-    
+    */
     //Connexion cn;
     //cn = new Connexion("Hopital","root","root");
     //MenuConnexion mconnexion = new MenuConnexion();
@@ -79,16 +78,72 @@ public class TestHopital {
       Scanner sc = new Scanner(System.in);
       
       //L'utilisateur choisi lui meme les possibilités ( à reproduire dans un Jtextfield! mais attention blindage !!
-      String rot= sc.nextLine();
-      ArrayList r = rech.rechercherInfirmier(rot);
-      for (int i = 0; i < r.size(); i++){
-              System.out.print(r.get(i));
+      /**
+        //Rechercher un docteur 
+      String spe= sc.nextLine();
+      String nom1= sc.nextLine();
+      String prenom1= sc.nextLine();
+      ArrayList r1 = rech.rechercherDocteur(spe,nom1,prenom1);
+      for (int i = 0; i < r1.size(); i++){
+              System.out.println(r1.get(i));
           }
-     
+      
+     //Rechercher un infirmier
+     String rot= sc.nextLine();
+     String nom2= sc.nextLine();
+     String prenom2= sc.nextLine();
+      ArrayList r2 = rech.rechercherInfirmier(rot,nom2,prenom2);
+      for (int i = 0; i < r2.size(); i++){
+              System.out.println(r2.get(i));
+          }
+          
+    //Rechercher un service
+     String nomService= sc.nextLine();
+     String nom3= sc.nextLine();
+     String prenom3= sc.nextLine();
+     ArrayList r3 = rech.rechercherService(nomService,nom3,prenom3);
+      for (int i = 0; i < r3.size(); i++){
+              System.out.println(r3.get(i));
+          }
     
->>>>>>> 38f170ed0f6b229221a3407e6e7b53d231ff48d8
-    }
-
+      /**         
+    //Rechercher un malade
+     String nom4= sc.nextLine();
+     String prenom4= sc.nextLine();
+     String mutuelle= sc.nextLine();
+     ArrayList r4 = rech.rechercherMalade(nom4,prenom4, mutuelle);
+      for (int i = 0; i < r4.size(); i++){
+              System.out.println(r4.get(i));
+          } 
+    
+      
+      //Rechercher un malade à partir d'un numero de chambre
+     String nom5= sc.nextLine();
+     String prenom5= sc.nextLine();
+     String no_chambre= sc.nextLine();
+     ArrayList r5 = rech.rechercherHospitalisation(nom5,prenom5, no_chambre);
+      for (int i = 0; i < r5.size(); i++){
+              System.out.println(r5.get(i));
+          } 
+     
+       //Rechercher les infos d'une info d'une chambre
+     String nb_lits= sc.nextLine(); //On recherche les chambres à partir du nombre de lits
+     String no_chambre= sc.nextLine(); //Recherche à partir du num de la chambre
+     ArrayList r6 = rech.rechercherChambre(nb_lits, no_chambre);
+      for (int i = 0; i < r6.size(); i++){
+              System.out.println(r6.get(i));
+          } 
+      */
+       //Rechercher les infos d'une info d'un patient soigné
+     String nom= sc.nextLine(); //On recherche la personne soignée a partir de son nom
+     String prenom= sc.nextLine(); //On recherche la personne soignée a partir de son prenom
+     String nomD= sc.nextLine(); //On recherche la personne soignée a partir du nom de son medecin
+     String prenomD= sc.nextLine(); //On recherche la personne soignée a partir du prenom de son medecin
+     ArrayList r7 = rech.rechercherSoigne(nom, prenom,nomD, prenomD);
+      for (int i = 0; i < r7.size(); i++){
+              System.out.println(r7.get(i));
+          } 
+}
 }
 
 
--- src/Modele/Recherche.java
+++ src/Modele/Recherche.java
@@ -32,25 +32,28 @@ public Recherche() throws ClassNotFoundException, SQLException{
 
 
 //Fonction pour rechercher un service selon le nom 
-public ArrayList rechercherService (String nom) throws SQLException, ClassNotFoundException{
+public ArrayList rechercherService (String nomService, String nom ,String prenom) throws SQLException, ClassNotFoundException{
 
      //Connexion cn = new Connexion("Hopital", "root", "root");
-     query= "SELECT * FROM service WHERE true";
-     if (!nom.equals("")){query+=" AND nom ='" + nom + "'";}
+     query= "SELECT * FROM employe INNER JOIN service WHERE true AND employe.numero=service.directeur";
+     if (!nom.equals("")){query+=" AND employe.nom ='" + nom + "'";}
+     if (!prenom.equals("")){query+=" AND employe.prenom ='" + prenom + "'";}
+     if (!nomService.equals("")){query+=" AND service.nom='" + nomService + "'";} //Je cherche les infos d'un service
      
-      try {System.out.print(query);
+      try {System.out.println(query);
           arr=cn.remplirChampsRequete(query);}catch(SQLException e)
       {e.printStackTrace();}
       return arr;
     }
          
 //Fonction pour rechercher une chambre selon le numéro et le nombre de lits
-public ArrayList rechercherChambre (String numero, String nb_chambres) throws SQLException, ClassNotFoundException{
+public ArrayList rechercherChambre (String nb_lits, String no_chambre) throws SQLException, ClassNotFoundException{
 
      //Connexion cn = new Connexion("Hopital", "root", "root");
-     query= "SELECT * FROM chambre WHERE true";
-     if (!numero.equals("")){query+=" AND no_chambre ='" + numero + "'";}
-     if (!nb_chambres.equals("")){query+=" AND nb_lits ='" + nb_chambres + "'";}
+     query= "SELECT * FROM employe INNER JOIN chambre WHERE true AND employe.numero=chambre.surveillant ";
+     if (!nb_lits.equals("")){query+=" AND chambre.nb_lits ='" + nb_lits + "'";}
+     if (!no_chambre.equals("")){query+=" AND chambre.no_chambre ='" + no_chambre + "'";}
+     
      
       try {System.out.println(query);
           arr=cn.remplirChampsRequete(query);}catch(SQLException e)
@@ -73,11 +76,16 @@ public ArrayList rechercherChambre (String numero, String nb_chambres) throws SQ
     }
 
    //Rechercher des docteurs selon la spécialité
-    public ArrayList rechercherDocteur (String spe) throws SQLException, ClassNotFoundException{
+    public ArrayList rechercherDocteur (String spe,String nom, String prenom) throws SQLException, ClassNotFoundException{
 
      //Connexion cn = new Connexion("Hopital", "root", "root");
-     query= "SELECT * FROM docteur WHERE true";
-     if (!spe.equals("")){query+=" AND specialite='" + spe + "'";}     
+      query= "SELECT * FROM employe WHERE true";
+     if (!nom.equals("")){query+=" AND nom ='" + nom + "'";}
+     if (!prenom.equals("")){query+=" AND prenom ='" + prenom + "'";}
+     if (!spe.equals("")){query+=" AND numero IN (SELECT numero FROM docteur WHERE specialite='" + spe + "'";} //Recherche à partir d'une spe
+     
+     /** query= "SELECT * FROM docteur WHERE true";
+     if (!spe.equals("")){query+=" AND specialite='" + spe + "'";}  */   
       try {System.out.println(query);
           arr=cn.remplirChampsRequete(query);}catch(SQLException e)
       {e.printStackTrace();}
@@ -86,16 +94,60 @@ public ArrayList rechercherChambre (String numero, String nb_chambres) throws SQ
     
     
     //Rechercher des infirmiers selon la rotation
-    public ArrayList rechercherInfirmier (String rot) throws SQLException, ClassNotFoundException{
+    public ArrayList rechercherInfirmier (String rot,String nom, String prenom) throws SQLException, ClassNotFoundException{
 
      //Connexion cn = new Connexion("Hopital", "root", "root");
-     query= "SELECT * FROM infirmier WHERE true";
-     if (!rot.equals("")){query+=" AND rotation='" + rot + "'";}     
+      //Connexion cn = new Connexion("Hopital", "root", "root");
+      query= "SELECT * FROM employe WHERE true";
+     if (!nom.equals("")){query+=" AND nom ='" + nom + "'";}
+     if (!prenom.equals("")){query+=" AND prenom ='" + prenom + "'";}
+     if (!rot.equals("")){query+=" AND numero IN (SELECT numero FROM infirmier WHERE rotation='" + rot + "'";} //Recherche à partir d'une spe
+      
       try {System.out.println(query);
           arr=cn.remplirChampsRequete(query);}catch(SQLException e)
       {e.printStackTrace();}
       return arr;
     }
+    //Rechercher un malade
+     public ArrayList rechercherMalade (String nom, String prenom, String mutuelle) throws SQLException, ClassNotFoundException{
+
+     //Connexion cn = new Connexion("Hopital", "root", "root");
+     query= "SELECT * FROM malade WHERE true";
+     if (!nom.equals("")){query+=" AND nom ='" + nom + "'";}
+     if (!prenom.equals("")){query+=" AND prenom ='" + prenom + "'";}
+     if (!mutuelle.equals("")){query+=" AND mutuelle ='" + mutuelle + "'";}
+      try {System.out.println(query);
+          arr=cn.remplirChampsRequete(query);}catch(SQLException e)
+      {e.printStackTrace();}
+      return arr;
+    }
+     //Recherche un patient hospitalisé 
+     public ArrayList rechercherHospitalisation (String nom, String prenom, String no_chambre) throws SQLException, ClassNotFoundException{
+     //Connexion cn = new Connexion("Hopital", "root", "root");
+     query= "SELECT * FROM malade INNER JOIN hospitalisation WHERE true AND malade.numero=hospitalisation.no_malade";
+     if (!nom.equals("")){query+=" AND malade.nom ='" + nom + "'";}
+     if (!prenom.equals("")){query+=" AND malade.prenom ='" + prenom + "'";}
+     if (!no_chambre.equals("")){query+=" AND hospitalisation.no_chambre='" + no_chambre + "'";} //Recherche à partir du numero de la chambre
+      try {System.out.println(query);
+          arr=cn.remplirChampsRequete(query);}catch(SQLException e)
+      {e.printStackTrace();}
+      return arr;
+    } 
+     
+      //Recherche un patient hospitalisé 
+     public ArrayList rechercherSoigne (String nom, String prenom, String nomD, String prenomD) throws SQLException, ClassNotFoundException{
+     //Connexion cn = new Connexion("Hopital", "root", "root");
+     query= "SELECT * FROM malade INNER JOIN employe INNER JOIN soigne WHERE true AND malade.numero=soigne.no_malade AND employe.numero=soigne.no_docteur";
+     if (!nom.equals("")){query+=" AND malade.nom ='" + nom + "'";}
+     if (!prenom.equals("")){query+=" AND malade.prenom ='" + prenom + "'";}
+     if (!nomD.equals("")){query+=" AND employe.nom ='" + nomD + "'";}
+     if (!prenomD.equals("")){query+=" AND employe.prenom ='" + prenomD + "'";}
+      try {System.out.println(query);
+          arr=cn.remplirChampsRequete(query);}catch(SQLException e)
+      {e.printStackTrace();}
+      return arr;
+    } 
+    
 }
 
 
