d75af38a598a2d776d8d63811b0c66433d9bd14a
claratromelin <clara.tromelin@edu.ece.fr>
2018-04-19 13:00:27 +0200

    mise a jour supprimer

--- src/Contrôleur/TestHopital.java
+++ src/Contrôleur/TestHopital.java
@@ -7,6 +7,7 @@ package Contrôleur;
 import java.sql.*;
 
 import Modele.Connexion;
+import Modele.Mise_a_jour;
 import Modele.Recherche;
 import Vue.FenChoixModule;
 import Vue.FenetreConnexionLocale;
@@ -69,14 +70,16 @@ public class TestHopital {
           }*/
 
       Recherche rech = new Recherche();
+      Mise_a_jour maj= new Mise_a_jour();
       Scanner sc = new Scanner(System.in);
       
       //L'utilisateur choisi lui meme les possibilités ( à reproduire dans un Jtextfield! mais attention blindage !!
-      String rot= sc.nextLine();
-      ArrayList r = rech.rechercherInfirmier(rot);
-      for (int i = 0; i < r.size(); i++){
-              System.out.print(r.get(i));
-          }
+      String nom = sc.nextLine();
+      maj.DeleteEmploye(nom);
+      //ArrayList r = rech.rechercherInfirmier(rot);
+      //for (int i = 0; i < r.size(); i++){
+              //System.out.print(r.get(i));
+         // }
      
     
     }
